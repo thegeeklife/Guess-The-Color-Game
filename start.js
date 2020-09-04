050b527abb1f65640495de67c455cf18068eeb10
Subia <subi@Subias-MacBook-Air.local>
2020-09-04 19:52:48 -0400

    start.js file added to host on Heroku

---
+++ start.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.use(express.static(path.join(__dirname)));
+
+
+// viewed at based directory http://localhost:8080/
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + '/colorGame.html'));
+});
+
+app.listen(process.env.PORT || 3000);
